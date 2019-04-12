dd4f6a7180a0e82e9a1481322f3dbba441995a86
sammy khaleel <sammykhaleel83@gmail.com>
2019-04-12 02:19:07 -0400

    redux Logout & The Error Reducer

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
 
 class Landing extends Component {
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
   render() {
     return (
       <div className="landing">
@@ -11,6 +19,7 @@ class Landing extends Component {
               <div className="col-md-12 text-center">
                 <h1 className="display-3 mb-4">Developer Connector</h1>
                 <p className="lead">
+                  {" "}
                   Create a developer profile/portfolio, share posts and get help
                   from other developers
                 </p>
@@ -18,7 +27,7 @@ class Landing extends Component {
                 <Link to="/register" className="btn btn-lg btn-info mr-2">
                   Sign Up
                 </Link>
-                <Link to="login" className="btn btn-lg btn-light">
+                <Link to="/login" className="btn btn-lg btn-light">
                   Login
                 </Link>
               </div>
@@ -29,4 +38,13 @@ class Landing extends Component {
     );
   }
 }
-export default Landing;
+
+Landing.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(Landing);
