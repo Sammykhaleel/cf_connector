073963005e99a733d49b09a9cdde7ba867f2b5e7
sammy khaleel <sammykhaleel83@gmail.com>
2019-04-20 16:17:55 -0400

    adding PrivateRoute for logout user

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,15 @@ import { clearCurrentProfile } from "./actions/profileActions";
 import jwt_decode from "jwt-decode";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
 import store from "./store";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Landing from "./components/layout/Landing";
 import Dashboard from "./components/dashboard/Dashboard";
+
+import PrivateRoute from "./components/common/PrivateRoute";
 
 import "./App.css";
 
@@ -49,7 +51,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
-              <Route exact path="/dashboard" component={Dashboard} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
             </div>
             <Footer />
           </div>
