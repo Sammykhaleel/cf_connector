d740fb348c2bec05eab00ffc5b46a89df994a715
sammy khaleel <sammykhaleel83@gmail.com>
2019-04-29 16:38:42 -0400

    debugging blank screen on mobile browser

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
+const path = require("path");
 
 const app = express();
 var cors = require("cors");
@@ -36,14 +37,12 @@ app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 // Server static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
